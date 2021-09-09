1ed2a941d1a733e43c2cfc113aef3d0e2c855ce3
unknown <greg.brosman@gmail.com>
2021-09-09 15:19:27 -0700

    Movie price calculated and seat count displayed

--- script.js
+++ script.js
@@ -8,7 +8,29 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 // (+) before movieSelect changes value to an int without having to use Parse.
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
+
+
+// Change the movie price value when different movie chosen from list.
+movieSelect.addEventListener('change', e=> {
+    ticketPrice = +e.target.value;
+    
+    updateSelectedCount()
+})
+
+
+
+
+// Get the length of the selected seats node list, change the text of the ticket count and total price.
+function updateSelectedCount(){
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
 
 // On click, check if it is a seat that is not occupied. If seat not occupied, toggle the seat to be selected and change the color.
 container.addEventListener('click', (e) => {
@@ -17,6 +39,8 @@ container.addEventListener('click', (e) => {
         !e.target.classList.contains('occupied')
     ) {
         e.target.classList.toggle('selected');
+
+        updateSelectedCount();
     }
 })
 
